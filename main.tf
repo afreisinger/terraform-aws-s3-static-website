a6dd6557941ac190ff0e002596dd695ecacf4804
Adrián Freisinger <afreisinger@gmail.com>
2024-07-21 20:52:47 -0300

    fix name

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "random_pet" "short" {
 # Locals
 #------------------------------------------------------------------------------
 locals {
-  website_bucket_name     = "${var.name_prefix}-source-${random_pet.short.id}"
-  www_website_bucket_name = "www.${var.name_prefix}-source-${random_pet.short.id}"
+  website_bucket_name     = var.website_domain_name
+  www_website_bucket_name = "www.${var.website_domain_name}"
 
   common_tags = {
     Author      = var.author
